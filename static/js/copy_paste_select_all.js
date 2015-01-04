9e96137e9162398f78bfda5577b2702529071e03
John McLear <john@mclear.co.uk>
2015-01-04 03:45:30 +0000

    support insensitive and also replace in urls

--- static/js/copy_paste_select_all.js
+++ static/js/copy_paste_select_all.js
@@ -20,36 +20,38 @@ exports.postAceInit = function(hook, context){
 }
 
 function findAndReplace(searchText, replacement, searchNode) {
-    if (!searchText || typeof replacement === 'undefined') {
-        // Throw error here if you want...
-        return;
+  if (!searchText || typeof replacement === 'undefined') {
+    // Throw error here if you want...
+    return;
+  }
+  var regex = typeof searchText === 'string' ?
+    new RegExp(searchText, 'gi') : searchText,
+    childNodes = (searchNode || document.body).childNodes,
+    cnLength = childNodes.length,
+    excludes = ["html","head","style","title","meta","script","object","iframe","link"];
+
+  while (cnLength--) {
+    var currentNode = childNodes[cnLength];
+    if (currentNode.nodeType === 1){
+      if(excludes.indexOf(currentNode.nodeName.toLowerCase() === -1)){
+        arguments.callee(searchText, replacement, currentNode);
+      }
     }
-    var regex = typeof searchText === 'string' ?
-                new RegExp(searchText, 'gi') : searchText,
-        childNodes = (searchNode || document.body).childNodes,
-        cnLength = childNodes.length,
-        excludes = 'html,head,style,title,link,meta,script,object,iframe';
-    while (cnLength--) {
-        var currentNode = childNodes[cnLength];
-        if (currentNode.nodeType === 1 &&
-            (excludes + ',').indexOf(currentNode.nodeName.toLowerCase() + ',') === -1) {
-            arguments.callee(searchText, replacement, currentNode);
-        }
-        if (currentNode.nodeType !== 3 || !regex.test(currentNode.data) ) {
-            continue;
-        }
-        var parent = currentNode.parentNode,
-            frag = (function(){
-                var html = currentNode.data.replace(regex, replacement),
-                    wrap = document.createElement('div'),
-                    frag = document.createDocumentFragment();
-                wrap.innerHTML = html;
-                while (wrap.firstChild) {
-                    frag.appendChild(wrap.firstChild);
-                }
-                return frag;
-            })();
-        parent.insertBefore(frag, currentNode);
-        parent.removeChild(currentNode);
+    if (currentNode.nodeType !== 3 || !regex.test(currentNode.data) ) {
+      continue;
     }
+    var parent = currentNode.parentNode,
+    frag = (function(){
+      var html = currentNode.data.replace(regex, replacement),
+      wrap = document.createElement('div'),
+      frag = document.createDocumentFragment();
+      wrap.innerHTML = html;
+      while (wrap.firstChild) {
+        frag.appendChild(wrap.firstChild);
+      }
+      return frag;
+    })();
+    parent.insertBefore(frag, currentNode);
+    parent.removeChild(currentNode);
+  }
 }
