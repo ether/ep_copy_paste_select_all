4440026d2e6c4b49d8a6039754949f1a083392f6
John McLear <john@mclear.co.uk>
2020-11-22 15:15:24 +0000

    autofixes from Etherpad checkPlugins.js

--- copy_paste_select_all.js
+++ copy_paste_select_all.js
@@ -1,9 +1,8 @@
-var eejs = require('ep_etherpad-lite/node/eejs/');
-var settings = require('ep_etherpad-lite/node/utils/Settings');
-var checked_state = '';
+const eejs = require('ep_etherpad-lite/node/eejs/');
+const settings = require('ep_etherpad-lite/node/utils/Settings');
+const checked_state = '';
 
 exports.eejsBlock_dd_edit = function (hook_name, args, cb) {
-  args.content = args.content + eejs.require('ep_copy_paste_select_all/templates/copy_paste_select_all_menu.ejs');
+  args.content += eejs.require('ep_copy_paste_select_all/templates/copy_paste_select_all_menu.ejs');
   return cb();
-}
-
+};
--- static/js/copy_paste_select_all.js
+++ static/js/copy_paste_select_all.js
@@ -1,50 +1,50 @@
-exports.postAceInit = function(hook, context){
-  $('#selectAll').click(function(){
-    context.ace.callWithAce(function(ace){
-      var document = ace.ace_getDocument();
+exports.postAceInit = function (hook, context) {
+  $('#selectAll').click(() => {
+    context.ace.callWithAce((ace) => {
+      const document = ace.ace_getDocument();
 
-      var numberOfLines=$(document).find("body").contents().length;
-      var lastLineLenght = 8;
-      ace.ace_performSelectionChange([0,0], [numberOfLines-1,0], false);
-    },'selectAll' , true);
+      const numberOfLines = $(document).find('body').contents().length;
+      const lastLineLenght = 8;
+      ace.ace_performSelectionChange([0, 0], [numberOfLines - 1, 0], false);
+    }, 'selectAll', true);
   });
 
-  $('#findAndReplace').click(function(){
-    var from = prompt("Search for...");
-    var to = prompt("Replace with...");
-    var HTMLLines = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find("#innerdocbody").children("div");
-    $(HTMLLines).each(function(){ // For each line
+  $('#findAndReplace').click(() => {
+    const from = prompt('Search for...');
+    const to = prompt('Replace with...');
+    const HTMLLines = $('iframe[name="ace_outer"]').contents().find('iframe').contents().find('#innerdocbody').children('div');
+    $(HTMLLines).each(function () { // For each line
       findAndReplace(from, to, this);
     });
   });
-}
+};
 
 function findAndReplace(searchText, replacement, searchNode) {
   if (!searchText || typeof replacement === 'undefined') {
     // Throw error here if you want...
     return;
   }
-  var regex = typeof searchText === 'string' ?
-    new RegExp(searchText, 'gi') : searchText,
-    childNodes = (searchNode || document.body).childNodes,
-    cnLength = childNodes.length,
-    excludes = ["html","head","style","title","meta","script","object","iframe","link"];
+  const regex = typeof searchText === 'string'
+    ? new RegExp(searchText, 'gi') : searchText;
+  const childNodes = (searchNode || document.body).childNodes;
+  let cnLength = childNodes.length;
+  const excludes = ['html', 'head', 'style', 'title', 'meta', 'script', 'object', 'iframe', 'link'];
 
   while (cnLength--) {
     var currentNode = childNodes[cnLength];
-    if (currentNode.nodeType === 1){
-      if(excludes.indexOf(currentNode.nodeName.toLowerCase() === -1)){
+    if (currentNode.nodeType === 1) {
+      if (excludes.indexOf(currentNode.nodeName.toLowerCase() === -1)) {
         arguments.callee(searchText, replacement, currentNode);
       }
     }
-    if (currentNode.nodeType !== 3 || !regex.test(currentNode.data) ) {
+    if (currentNode.nodeType !== 3 || !regex.test(currentNode.data)) {
       continue;
     }
-    var parent = currentNode.parentNode,
-    frag = (function(){
-      var html = currentNode.data.replace(regex, replacement),
-      wrap = document.createElement('div'),
-      frag = document.createDocumentFragment();
+    const parent = currentNode.parentNode;
+    const frag = (function () {
+      const html = currentNode.data.replace(regex, replacement);
+      const wrap = document.createElement('div');
+      const frag = document.createDocumentFragment();
       wrap.innerHTML = html;
       while (wrap.firstChild) {
         frag.appendChild(wrap.firstChild);
